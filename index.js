1aa5d93b9742efd33cfeb980557755bed2cc84e5
farhadctg22201~ <farhadctg222@gmail.com>
2023-07-18 09:24:30 -0700

    d

--- index.js
+++ index.js
@@ -24,42 +24,81 @@
 
 // const xy = (c,d) => c+ d 
 // console.log(xy(11,22)) 
-const frn = [11,2,1,3,4,5,6,7,8]
-const fahad = [22,33,44,55,66,77,88,1000]
-const all = [...frn,...fahad]
-const a = 20
-const b = 3000
-const c = 200
-const d = 100
-const max = Math.max(...all)
-console.log(max)
-class fulname{
-    constructor(){
+// const frn = [11,2,1,3,4,5,6,7,8]
+// const fahad = [22,33,44,55,66,77,88,1000]
+// const all = [...frn,...fahad]
+// const a = 20
+// const b = 3000
+// const c = 200
+// const d = 100
+// const max = Math.max(...all)
+// console.log(max)
+// class fulname{
+//     constructor(){
         
-        this.school = 'satkania high school'
-        this.join =  Date(2012)
-    }
-}
+//         this.school = 'satkania high school'
+//         this.join =  Date(2012)
+//     }
+// }
 
-class totall extends fulname {
+// class totall extends fulname {
     
-    constructor(name,age) {
-        super()
-        this.name = name
-        this.age = age
+//     constructor(name,age) {
+//         super()
+//         this.name = name
+//         this.age = age
     
+//     }
+    
+//   }
+//   const fulname1 = new totall('farhad',2019)
+//   const fulname2 = new totall('rakib',2019)
+//   const fulname3 = new totall('osman',2023)
+
+// //   const alll = fulname.year()
+//   console.log(fulname1)
+//   console.log(fulname2)
+//   console.log(fulname3)
+  const farhad = {
+    name: 'farhad',
+    age: 20,
+    collage: 'satkania collage',
+    live: 'chittagong',
+    join: 2023,
+    details:{
+      adrees: 'satkania',
+      word: '9no word'
     }
-    
   }
-  const fulname1 = new totall('farhad',2019)
-  const fulname2 = new totall('rakib',2019)
-  const fulname3 = new totall('osman',2023)
+  // const name = farhad.name
+  // const age = farhad.age
+  const {word,adrees} = farhad.details
+  console.log(word,adrees)
+  console.log()
+  const array = ['emdad','soloman','saifudding','nazim','azim']
+  const [fist,second,...last]= array
+  console.log(last)
+  class esto {
+    constructor(){
+      this.esto = 1999
+    }
+  }
 
-//   const alll = fulname.year()
-  console.log(fulname1)
-  console.log(fulname2)
-  console.log(fulname3)
-  
+  class myfrind extends esto {
+    constructor(name,school){
+      super() 
+this.name = name
+this.school = school
+
+    }
+  }
+  const abc = new myfrind('smith win','satkania school')
+  const ab = new myfrind('mohammad','gopur ga school')
+
+  console.log(abc)
+  console.log(ab)
+
+
 
 // console.log(alll)
 // console.log(fulname1)
